0b2e7376aa7f2cd176ff41b278e8b722791af010
LorenaAgustinaLujan <lujan.agustina@hotmail.com>
2022-09-23 17:06:36 -0300

    Creacion de carpeta Frontend
    Se creo la carpeta FrontEnd

---
+++ my-app/FRONTEND/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDEComponent } from './components/acerca-de/acerca-de.component';
+import { PorfolioService } from '../environments/servicios/porfolio.service';
+import {HttpClientModule} from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDEComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
