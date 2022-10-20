096ff79b33e7ee28616065f1021749fd0c41376a
LorenaAgustinaLujan <lujan.agustina@hotmail.com>
2022-10-20 00:59:21 -0300

    commit API
    guardo los cambios de netbeans hechos hasta la prueba en postman

--- BACKEND/backendLA/src/main/java/com/backend/backendLA/Security/jwt/JwtEntryPoint.java
+++ BACKEND/backendLA/src/main/java/com/backend/backendLA/Security/jwt/JwtEntryPoint.java
@@ -4,10 +4,25 @@
  */
 package com.backend.backendLA.Security.jwt;
 
-/**
- *
- * @author Usuario
- */
-public class JwtEntryPoint {
-    
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+
+    private final static Logger logger = LoggerFactory.getLogger(JwtEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        logger.error("Fallo el metodo commence");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
 }
